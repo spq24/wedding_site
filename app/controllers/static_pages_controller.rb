92d7cbbf90cbe9cd02c76e74b31fbd8a1b3167f8
spq24 <spq2461@gmail.com>
2015-04-05 05:44:53 +0000

    initial commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,8 @@
+class StaticPagesController < ApplicationController
+
+	def home
+    response = HTTParty.get('https://api.instagram.com/v1/tags/allyqwedding/media/recent?&access_token=182837634.5955ee4.23903dd923b445a79639d405ea6729d6')
+    @tags = JSON.parse(response.body)["data"]
+	end
+	
+end
---
+++ config/initializers/instagram.rb
@@ -0,0 +1,7 @@
+require "instagram"
+ 
+ Instagram.configure do |config|
+    config.client_id = "5955ee46b6a94a98858da96ce6fddf04"
+    config.access_token = "182837634.5955ee4.23903dd923b445a79639d405ea6729d6"
+ end
+

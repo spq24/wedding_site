5517dddfb7d5aae7d7f4165d5e02cf2e3a7432f0
Steve Quatrani <spq2461@gmail.com>
2015-04-06 14:34:10 -0400

    setting up redirect of naked domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def check_uri
+    redirect_to request.protocol + "www." + request.host_with_port + request.request_uri if !/^www/.match(request.host)
+  end
 end
